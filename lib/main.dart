983a8ab653bd8a89693f9fd1664ba071ce204a07
RashadZA <97411218+RashadZA@users.noreply.github.com>
2024-01-21 18:28:55 +0600

    Firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:whatsappc/presentation/startupScreens/splash_screen.dart';
 import 'package:whatsappc/utils/design_utils.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
